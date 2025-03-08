97a4e4bdc1d826faaa6a4de1fd7af9b0ff638795
Andy Stetson <stetson.a@gmail.com>
2025-03-07 23:32:28 -0500

    work around signing?

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,10 +108,6 @@ publishing {
 }
 
 signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PASSPHRASE")
-    )
     sign(publishing.publications)
 }
 
